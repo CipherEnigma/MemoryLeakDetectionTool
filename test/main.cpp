130cce5e75d0f130ef85fb1d8c1fb567ede2336a
Anushi <153729522+anushi2511@users.noreply.github.com>
2025-04-20 18:45:10 +0530

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -2,27 +2,24 @@
 #include <iostream>
 
 int main() {
-    // Allocate memory using TRACK_NEW
     int* intPtr = TRACK_NEW(int);
     *intPtr = 42;
 
-    // Allocate an array using TRACK_NEW_ARRAY
     int* intArray = TRACK_NEW_ARRAY(int, 5);
     for (int i = 0; i < 5; ++i) {
         intArray[i] = i * 10;
     }
 
-    // Intentionally forget to deallocate intPtr to simulate a memory leak
-
-    // Properly deallocate intArray
     TRACK_DELETE_ARRAY(intArray);
 
     // Allocate another object
     double* doublePtr = TRACK_NEW(double);
     *doublePtr = 3.14;
 
-    // Forget to deallocate doublePtr to simulate another memory leak
+    char* charArray = TRACK_NEW_ARRAY(char, 10);
+    for (int i = 0; i < 10; ++i) {
+        charArray[i] = 'A' + i;
+    }
 
-    // Program ends here, and MemoryTracker will automatically report leaks
     return 0;
 }
